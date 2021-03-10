658c4c475ecad0ca38aa6e33c2e7c4c1738ded71
XamuelUG <samweko@gmail.com>
2021-03-10 16:00:24 +0300

    configured hbs

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-
+const hbs = require("hbs");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3500;
-app.get("/", (req, res) => {
-  res.send("Hello Xamuel");
+
+app.set("view engine", "hbs");
+
+app.get("/", function (req, res) {
+  res.render("home");
 });
+
 app.listen(PORT, () => {
   console.log(`Server started on Port ${PORT} ....`);
 });
