e2bb3b8b45a70aab107a4e836fc3c40ca7d256f1
lukenge-lukard <lukengelukard@gmail.com>
2021-03-10 14:10:09 +0300

    changed xamuel to campus

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
 app.get("/", (req, res) => {
-  res.send("Hello Xamuel");
+  res.send("Hello Campus");
 });
 app.listen(PORT, () => {
   console.log(`Server started on Port ${PORT} ....`);
