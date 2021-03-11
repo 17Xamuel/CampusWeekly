fa63a065d0deccf05b28caf9690b459b46d83182
lukenge-lukard <lukengelukard@gmail.com>
2021-03-11 13:07:15 +0300

    worked on the new footer and header for all the pages, plus their navigation

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const express = require("express");
 const hbs = require("hbs");
 const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3600;
+
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory));
 
 app.set("view engine", "hbs");
 
 app.get("/", function (req, res) {
   res.render("home");
+});
+app.get("/lifestyle", function (req, res) {
+  res.render("lifestyle");
+});
+app.get("/carrier", function (req, res) {
+  res.render("carrier");
+});
+app.get("/relationships", function (req, res) {
+  res.render("relationships");
+});
+app.get("/personalities", function (req, res) {
+  res.render("personalities");
 });
 
 app.listen(PORT, () => {
